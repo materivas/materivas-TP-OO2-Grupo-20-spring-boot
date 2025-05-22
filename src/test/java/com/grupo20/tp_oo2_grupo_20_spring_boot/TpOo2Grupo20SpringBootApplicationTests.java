5b59e2f201cf66abd6d7b3afa5fec46508394aae
materivas <matiensoriv@gmail.com>
2025-05-22 18:56:08 -0300

    Creación del proyecto

---
+++ src/test/java/com/grupo20/tp_oo2_grupo_20_spring_boot/TpOo2Grupo20SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo20.tp_oo2_grupo_20_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpOo2Grupo20SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
