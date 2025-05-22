cc339e056cefca9bf8935ef319031081ac000b99
materivas <matiensoriv@gmail.com>
2025-05-22 19:03:58 -0300

    Creación del Proyecto en Spring

---
+++ src/main/java/com/grupo20/demo/TpOo2Grupo20SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.grupo20.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpOo2Grupo20SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpOo2Grupo20SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo20/demo/TpOo2Grupo20SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo20.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpOo2Grupo20SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
